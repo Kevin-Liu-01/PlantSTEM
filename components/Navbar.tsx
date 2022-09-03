3f018175d5b5243967b8549d181f73a9f27ace97
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2022-09-03 00:00:59 -0400

    navbar fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,7 @@ function Navbar(props) {
               <div className="relative flex items-center justify-between h-16">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center pr-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md text-gray-400 hover:text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -308,7 +308,7 @@ function Navbar(props) {
               </div>
             </div>
             <Disclosure.Panel className="sm:hidden ">
-              <div className="px-2 pt-2 pb-3 space-y-1 ">
+              <div className="mx-7 md:px-2 pt-2 pb-3 space-y-1 ">
                 {navigation.map((item) =>
                   item.name != "Classes" ? (
                     <Disclosure.Button
@@ -317,7 +317,7 @@ function Navbar(props) {
                       href={item.href}
                       className={classNames(
                         item.name === props.page
-                          ? "hover:bg-gray-300 text-gray-700 w-[30%] border border-t-0 border-l-0 border-r-0 border-b-4 border-plantGreen"
+                          ? "hover:bg-gray-300 text-gray-700 border border-t-0 border-l-0 border-r-0 border-b-4 border-plantGreen"
                           : "text-gray-400 hover:bg-gray-300 hover:text-gray-700",
                         "block px-3 py-2 text-base font-medium hover:scale-105 transition duration-200 ease-in-out"
                       )}
