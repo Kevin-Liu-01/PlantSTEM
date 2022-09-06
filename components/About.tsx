79e09e3a10e216bfa8e9343208e0cef9063ae0b8
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2022-09-06 12:16:08 -0400

    Fix about page styling

--- components/About.tsx
+++ components/About.tsx
@@ -29,9 +29,9 @@ function About() {
               we are committed to teach anyone, regardless of their backgrounds.
             </p>
           </div>
-          <div className="max-w-7xl mx-auto border border-x-0 border-t-0 border-b-2 border-gray-300 mt-10"></div>
+          <div className="border border-x-0 border-t-0 border-b-2 border-gray-300 mt-10"></div>
           <div className="mt-12 rounded-xl">
-            <dl className=" mx-auto max-w-7xl m-4 space-y-10 md:grid md:grid-rows-3 md:gap-x-4 md:gap-y-10 md:space-y-0">
+            <dl className="m-4 space-y-10 md:grid md:grid-rows-3 md:gap-x-4 md:gap-y-10 md:space-y-0">
               <div className="relative">
                 <dt>
                   <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-plantGreen text-white">
